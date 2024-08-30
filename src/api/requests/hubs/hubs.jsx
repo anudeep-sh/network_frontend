2fce7f341b8375325c09f34649d4f95340f2cbc1
anudeep-sh <anudeep@optigrit.in>
2024-08-30 12:40:49 +0530

    Add files via upload

---
+++ src/api/requests/hubs/hubs.jsx
@@ -0,0 +1,54 @@
+import performRequestBackend from "../../axios";
+
+export const hub = {
+  addMember: async (shortcode,level) => {
+    const response = await performRequestBackend("/network", "POST",{shortcode,level});
+    return response;
+  },
+  getHubs: async () => {
+    const response = await performRequestBackend("/get-hub", "GET");
+    return response;
+  },
+  getQuotas: async() =>{
+    const user_details = localStorage.getItem("user_details")
+    const userId = JSON.parse(user_details)
+    const response = await performRequestBackend(`/get-quota/${userId?.[0]?.id}`,"GET")
+    return response
+  },
+  getQuota: async () => {
+    const response = await performRequestBackend("/get-quotas", "GET");
+    return response;
+  },
+  getWithdrawals: async () => {
+    const response = await performRequestBackend("/withdrawals-list/PENDING", "GET");
+    return response;
+  },
+  getApprovedWithdrawals: async () => {
+    const response = await performRequestBackend("/withdrawals-list/APPROVED", "GET");
+    return response;
+  },
+  getUsersWallet: async () => {
+    const response = await performRequestBackend("/users/wallet-level", "GET");
+    return response;
+  },
+  getUsersDetails: async () => {
+    const response = await performRequestBackend("/v1/users-details-by-id", "GET");
+    return response;
+  },
+  postQuota: async (body) => {
+    const response = await performRequestBackend("/post-quota", "POST",body);
+    return response;
+  },
+  patchWithdrawalStatusUpdate: async (body) => {
+    const response = await performRequestBackend("/update-withdrawal-request", "PATCH",body);
+    return response;
+  },
+  patchUpdateUserDetails: async (body) => {
+    const response = await performRequestBackend("/update-bank-details", "PATCH",body);
+    return response;
+  },
+  patchUpdateUserPassword: async (id,body) => {
+    const response = await performRequestBackend(`users/${id}/password`, "PATCH",body);
+    return response;
+  },
+};
---
+++ src/utils/dashBoardWidgetElements.js
@@ -0,0 +1,62 @@
+import Profile from "../Assets/Images/person.svg";
+import Wallet from "../Assets/Images/wallet.svg";
+import Income from "../Assets/Images/income.svg";
+import Withdrawal from "../Assets/Images/withdrawal.svg";
+
+export const widgetElements = [
+  {
+    icon: Profile,
+    title: "Customers Id",
+    value: "123",
+  },
+  {
+    icon: Wallet,
+    title: "Wallet",
+    value: "Rs. 1000",
+  },
+  {
+    icon: Income,
+    title: "Total Income",
+    value: "Rs. 2000",
+  },
+  {
+    icon: Withdrawal,
+    title: "Pending Withdrawal",
+    value: "Rs. 500",
+  },
+  //   {
+  //     icon: <AttachMoney />,
+  //     title: "Last Deposit",
+  //     value: "Rs. 500",
+  //   },
+  //   {
+  //     icon: <CurrencyExchange />,
+  //     title: "Last Withdrawal",
+  //     value: "Rs. 500",
+  //   },
+  //   {
+  //     icon: <CurrencyExchange />,
+  //     title: "Total Withdrawal",
+  //     value: "Rs. 500",
+  //   },
+  //   {
+  //     icon: <Diversity3 />,
+  //     title: "Total Referrals",
+  //     value: "10",
+  //   },
+  //   {
+  //     icon: <Diversity1 />,
+  //     title: "Pair Income",
+  //     value: "20",
+  //   },
+  //   {
+  //     icon: <CurrencyRupee />,
+  //     title: "Referral Income",
+  //     value: "Rs. 2000",
+  //   },
+  //   {
+  //     icon: <PersonAddAlt />,
+  //     title: "Direct Referrals",
+  //     value: "5",
+  //   },
+];
